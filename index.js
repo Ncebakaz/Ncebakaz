94d9c6eec439363070c2ba47d8ff919201e7a76e
Ncebakazi Somdaka <109283030+Ncebakaz@users.noreply.github.com>
2025-08-29 19:14:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+// Scroll Progress Bar
+window.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY;
+  let docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  let scrollPercent = (scrollTop / docHeight) * 100;
+  document.getElementById("progress-bar").style.width = scrollPercent + "%";
+});
